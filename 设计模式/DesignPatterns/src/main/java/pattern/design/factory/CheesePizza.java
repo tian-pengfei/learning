02c7eacf1d41d0120ec87f9c5a52a46926ff3f12
tianpengfei <tianpengfei@agree.com.cn>
2020-12-20 23:05:24 +0800

    1234

--- 设计模式/DesignPatterns/src/main/java/pattern/design/factory/CheesePizza.java
+++ 设计模式/DesignPatterns/src/main/java/pattern/design/factory/CheesePizza.java
@@ -8,10 +8,7 @@ import java.util.Comparator;
 public class CheesePizza extends Pizza{
 
     public static void main(String[] args) {
-        Path first = Paths.get("user/bin");
-                Comparator<String> comp =
-                        (first,second)-> first.length() - second.length() ;
-
+        Integer
     }
 
 }
