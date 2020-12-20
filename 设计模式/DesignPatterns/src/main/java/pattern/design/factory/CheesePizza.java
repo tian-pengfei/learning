eae811ec07169ad9a4b7640486ff70cb4c569261
tianpengfei <tianpengfei@agree.com.cn>
2020-12-20 14:49:59 +0800

    nihao

--- 设计模式/DesignPatterns/src/main/java/pattern/design/factory/CheesePizza.java
+++ 设计模式/DesignPatterns/src/main/java/pattern/design/factory/CheesePizza.java
@@ -1,4 +1,17 @@
 package pattern.design.factory;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+
+
 public class CheesePizza extends Pizza{
+
+    public static void main(String[] args) {
+        Path first = Paths.get("user/bin");
+                Comparator<String> comp =
+                        (first,second)-> first.length() - second.length() ;
+
+    }
+
 }
